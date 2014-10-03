9ae51cab43bb4e09cb1c4b2062d43214a3989570
Jason Heiss <jheiss@aput.net>
2014-10-02 21:27:50 -0400

    Allow user to change prompting file descriptor
    Primarily useful if the user wants to be prompted on stderr instead of
    stdout, if stdout is redirected to a file for example.

--- lib/citibike_trips.rb
+++ lib/citibike_trips.rb
@@ -3,15 +3,17 @@ require 'citibike_trips/station'
 require 'citibike_trips/stations'
 require 'citibike_trips/trip'
 require 'citibike_trips/trips'
-require 'highline/import'
+require 'highline'
 require 'mechanize'
 
 module CitibikeTrips
   LOGIN_URL = 'https://www.citibikenyc.com/login'
-  def self.login(page)
+  def self.login(page, options={})
+    stdout = options[:stdout] || $stdout
+    h = HighLine.new($stdin, stdout)
     page.form_with(action: LOGIN_URL) do |form|
-      form.subscriberUsername = ask('Citi Bike username: ')
-      form.subscriberPassword = ask('Citi Bike password: ') {|q| q.echo = false}
+      form.subscriberUsername = h.ask('Citi Bike username: ')
+      form.subscriberPassword = h.ask('Citi Bike password: ') {|q| q.echo = false}
       form.click_button(form.button_with(name: 'login_submit'))
     end
   end
